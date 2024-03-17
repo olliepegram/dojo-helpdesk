cb6c8cd9495ffdfc35c63665bb8d12e1ecd5ef65
olliepegram <olliepegram@gmail.com>
2024-03-17 16:42:15 +1100

    initial commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from './dojo-logo.png';
+
+function Navbar() {
+	return (
+		<nav>
+			<Image
+				src={Logo}
+				alt='Dojo Helpdesk logo'
+				width={70}
+				quality={100}
+				placeholder='blur'
+			/>
+			<h1>DOJO Helpdesk</h1>
+			<Link href='/'>Dashboard</Link>
+			<Link href='/tickets'>Tickets</Link>
+		</nav>
+	);
+}
+
+export default Navbar;
